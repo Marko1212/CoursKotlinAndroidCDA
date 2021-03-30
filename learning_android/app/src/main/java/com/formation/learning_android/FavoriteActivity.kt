641473205ed14a95da8a0a0a4d22241b54420309
Marko1212 <shihonage7@gmail.com>
2021-03-30 15:59:28 +0200

    update

---
+++ learning_android/app/src/main/java/com/formation/learning_android/FavoriteActivity.kt
@@ -0,0 +1,17 @@
+package com.formation.learning_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+
+class FavoriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorite)
+
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+}
