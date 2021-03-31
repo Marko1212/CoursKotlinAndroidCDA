611ef7fbcc149913e51e8043b88c6d42a8d12962
Marko1212 <shihonage7@gmail.com>
2021-03-31 12:10:11 +0200

    update

---
+++ AndroidMovies/app/src/main/java/com/formation/androidmovies/ViewOneMovie.kt
@@ -0,0 +1,11 @@
+package com.formation.androidmovies
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewOneMovie : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_one_movie)
+    }
+}
