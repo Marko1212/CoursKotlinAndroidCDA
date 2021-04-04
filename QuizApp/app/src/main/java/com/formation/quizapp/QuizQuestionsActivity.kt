c4900b4e0bf48dec6cc558887783fabdd5c494de
Marko1212 <shihonage7@gmail.com>
2021-04-04 21:28:54 +0200

    next

--- QuizApp/app/src/main/java/com/formation/quizapp/QuizQuestionsActivity.kt
+++ QuizApp/app/src/main/java/com/formation/quizapp/QuizQuestionsActivity.kt
@@ -1,32 +1,66 @@
 package com.formation.quizapp
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 
-class QuizQuestionsActivity : AppCompatActivity() {
+class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var mCurrentPosition:Int = 1
+
+    private var mQuestionsList: ArrayList<Question>? = null
+
+    private var mSelectedOptionPosition: Int = 0
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
 
-        val questionsList = Constants.getQuestions()
+        mQuestionsList = Constants.getQuestions()
 
-        Log.i("Questions Size", "${questionsList.size}")
+        setQuestion()
 
-        val currentPosition = 1
-        val question: Question? = questionsList[currentPosition - 1]
+        val tvOptionOne = findViewById<TextView>(R.id.tv_option_one)
+        tvOptionOne.setOnClickListener(this)
+
+        val tvOptionTwo = findViewById<TextView>(R.id.tv_option_two)
+        tvOptionTwo.setOnClickListener(this)
+
+        val tvOptionThree = findViewById<TextView>(R.id.tv_option_three)
+        tvOptionThree.setOnClickListener(this)
+
+        val tvOptionFour = findViewById<TextView>(R.id.tv_option_four)
+        tvOptionFour.setOnClickListener(this)
+
+
+
+
+    }
+
+    private fun setQuestion() {
+
+        mCurrentPosition = 1
+        val question = mQuestionsList!![mCurrentPosition - 1]
+
+        defaultOptionsView()
 
         val progressBar = findViewById<ProgressBar>(R.id.progressBar)
 
-        progressBar.progress = currentPosition
+        progressBar.progress = mCurrentPosition
 
         val tvProgress = findViewById<TextView>(R.id.tv_progress)
 
-        tvProgress.text = "$currentPosition" + "/" + progressBar.max
+        tvProgress.text = "$mCurrentPosition" + "/" + progressBar.max
 
         val tvQuestion = findViewById<TextView>(R.id.tv_question)
 
@@ -48,16 +82,32 @@ class QuizQuestionsActivity : AppCompatActivity() {
         val tvOptionFour = findViewById<TextView>(R.id.tv_option_four)
         tvOptionFour.text = question.optionFour
 
-        
 
+    }
 
+    private fun defaultOptionsView() {
+        val options = ArrayList<TextView>()
 
+        val tvOptionOne = findViewById<TextView>(R.id.tv_option_one)
+        val tvOptionTwo = findViewById<TextView>(R.id.tv_option_two)
+        val tvOptionThree = findViewById<TextView>(R.id.tv_option_three)
+        val tvOptionFour = findViewById<TextView>(R.id.tv_option_four)
 
+        options.add(0, tvOptionOne)
+        options.add(1, tvOptionTwo)
+        options.add(2, tvOptionThree)
+        options.add(3, tvOptionFour)
 
+        for (option in options) {
+            option.setTextColor(Color.parseColor("#7A8089"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(this, R.drawable.default_option_border_bg)
 
+        }
 
+    }
 
-
-
+    override fun onClick(v: View?) {
+        TODO("Not yet implemented")
     }
 }
