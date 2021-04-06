dcc2720fbc9837cbbc788da87615891d11a235ba
Marko1212 <shihonage7@gmail.com>
2021-04-06 16:57:00 +0200

    update

--- Notepad/app/src/main/java/com/formation/notepad/NoteDetailActivity.kt
+++ Notepad/app/src/main/java/com/formation/notepad/NoteDetailActivity.kt
@@ -1,27 +1,37 @@
 package com.formation.notepad
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 
 class NoteDetailActivity : AppCompatActivity() {
 
     companion object {
+        val REQUEST_EDIT_NOTE = 1
         val EXTRA_NOTE = "note"
         val EXTRA_NOTE_INDEX = "noteIndex"
     }
 
     lateinit var note: Note
+    var noteIndex: Int = -1
 
-    var noteIndex:Int = -1
-
-    lateinit var title:TextView
+    lateinit var title: TextView
     lateinit var text: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_detail)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
         title = findViewById(R.id.title)
         text = findViewById(R.id.text)
@@ -31,8 +41,55 @@ class NoteDetailActivity : AppCompatActivity() {
 
         title.text = note.title
         text.text = note.text
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.activity_note_detail, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.action_save -> {
+                saveNote()
+                true
+            }
+            R.id.action_delete -> {
+                showConfirmDeleteNoteDialog()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun showConfirmDeleteNoteDialog() {
+        val confirmDialog = ConfirmDeleteDialogFragment(note.title)
+        confirmDialog.listener = object : ConfirmDeleteDialogFragment.ConfirmDeleteDialogListener {
+            override fun onDialogPositiveClick() {
+                deleteNote()
+            }
+
+            override fun onDialogNegativeClick() {
+
+            }
+        }
+            confirmDialog.show(supportFragmentManager, "confirmDeleteDialog")
 
 
+    }
 
+    private fun deleteNote() {
+
+    }
+
+    private fun saveNote() {
+        note.title = title.text.toString()
+        note.text = text.text.toString()
+
+        intent = Intent()
+        intent.putExtra(EXTRA_NOTE, note)
+        intent.putExtra(EXTRA_NOTE_INDEX, noteIndex)
+        setResult(Activity.RESULT_OK, intent)
+        finish()
     }
 }
--- Notepad/app/src/main/java/com/formation/notepad/NoteListActivity.kt
+++ Notepad/app/src/main/java/com/formation/notepad/NoteListActivity.kt
@@ -1,36 +1,44 @@
 package com.formation.notepad
 
+import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class NoteListActivity : AppCompatActivity(), View.OnClickListener {
-    
-    lateinit var notes : MutableList<Note>
+
+    lateinit var notes: MutableList<Note>
     lateinit var adapter: NoteAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_list)
 
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        findViewById<FloatingActionButton>(R.id.create_note_fab).setOnClickListener(this)
+
         notes = mutableListOf()
         notes.add(Note("Note 1", "Blablablabla"))
         notes.add(Note("Course", "ne pas oublier de prendre des biscuits"))
         notes.add(
-            Note(
-                "Android Kotlin",
-                "C'est cool de faire de l'android, même si ce n'est pas toujours simple"
-            )
+                Note(
+                        "Android Kotlin",
+                        "C'est cool de faire de l'android, même si ce n'est pas toujours simple"
+                )
         )
         notes.add(
-            Note(
-                "Flutter",
-                "C'est cool aussi d'utiliser un framework pour coder une application sur différents appareils"
-            )
+                Note(
+                        "Flutter",
+                        "C'est cool aussi d'utiliser un framework pour coder une application sur différents appareils"
+                )
         )
 
         adapter = NoteAdapter(notes, this)
@@ -38,21 +46,55 @@ class NoteListActivity : AppCompatActivity(), View.OnClickListener {
         val recyclerView = findViewById<RecyclerView>(R.id.notes_recycler_view)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = adapter
-
     }
 
     override fun onClick(v: View) {
-        if (v.tag != null) {
+        if (v.tag != null){
             showNoteDetail(v.tag as Int)
+        } else {
+            when(v.id) {
+                R.id.create_note_fab->createNewNote()
+            }
         }
     }
 
+    private fun createNewNote() {
+        showNoteDetail(-1)
+    }
+
     private fun showNoteDetail(position: Int) {
-    val note = notes[position]
+        val note = if (position < 0) Note() else notes[position]
+
         val intent = Intent(this, NoteDetailActivity::class.java)
         intent.putExtra(NoteDetailActivity.EXTRA_NOTE, note)
         intent.putExtra(NoteDetailActivity.EXTRA_NOTE_INDEX, position)
-        startActivity(intent)
+        startActivityForResult(intent, NoteDetailActivity.REQUEST_EDIT_NOTE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode != Activity.RESULT_OK || data == null){
+            return
+        }
+
+        when(requestCode){
+            NoteDetailActivity.REQUEST_EDIT_NOTE -> processEditNoteResult(data)
+        }
+
+    }
+
+    private fun processEditNoteResult(data: Intent) {
+        val note = data.getParcelableExtra<Note>(NoteDetailActivity.EXTRA_NOTE)!!
+        val noteIndex = data.getIntExtra(NoteDetailActivity.EXTRA_NOTE_INDEX, -1)
+        saveNote(note, noteIndex)
+    }
+
+    private fun saveNote(note: Note, noteIndex: Int) {
+        if (noteIndex < 0) {
+            notes.add(0, note)
+        } else {
+            notes[noteIndex] = note
+        }
+        adapter.notifyDataSetChanged()
     }
 }
-
