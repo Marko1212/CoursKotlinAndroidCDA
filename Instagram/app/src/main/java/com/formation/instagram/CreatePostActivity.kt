6cd687955952b4fe849d65c98a28c4de939c6863
Marko1212 <shihonage7@gmail.com>
2021-04-09 10:31:19 +0200

    updates

--- Instagram/app/src/main/java/com/formation/instagram/CreatePostActivity.kt
+++ Instagram/app/src/main/java/com/formation/instagram/CreatePostActivity.kt
@@ -1,8 +1,12 @@
 package com.formation.instagram
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.View
 import android.widget.Button
@@ -14,13 +18,14 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class CreatePostActivity : AppCompatActivity(), View.OnClickListener {
 
+    lateinit var sp: SharedPreferences
     lateinit var bottomNavigationView: BottomNavigationView
 
     lateinit var nickname: TextView
     lateinit var description: EditText
     lateinit var descriptionCount: TextView
     lateinit var btnOpenGallery: Button
-    lateinit var photBlur: ImageView
+    lateinit var photoBlur: ImageView
     lateinit var photo: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,10 +34,44 @@ class CreatePostActivity : AppCompatActivity(), View.OnClickListener {
 
         bottomNavigationViewAction()
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener(this)
+
+        sp = getSharedPreferences("User", Context.MODE_PRIVATE)
+
+        nickname = findViewById(R.id.nickname)
+        description = findViewById(R.id.description)
+        descriptionCount = findViewById(R.id.description_count)
+        btnOpenGallery = findViewById(R.id.btn_open_gallery)
+        photoBlur = findViewById(R.id.photo_blur)
+        photo = findViewById(R.id.photo)
+
+        nickname.text = sp.getString("nickname", "")
+        getDescriptionForTextLength()
+
+
+
     }
 
-    override fun onClick(v: View?) {
+    override fun onClick(v: View) {
 
+    }
+
+    private fun getDescriptionForTextLength() {
+
+        descriptionCount.text = getString(R.string.description_text_length, 0)
+        description.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            descriptionCount.text = getString(R.string.description_text_length, s?.length)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        })
     }
 
     private fun bottomNavigationViewAction(){
