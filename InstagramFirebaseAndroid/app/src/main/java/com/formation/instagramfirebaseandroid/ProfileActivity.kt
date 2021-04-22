1ded426d3e6b0f5aaf2ca8f785d37af443152218
Marko1212 <shihonage7@gmail.com>
2021-04-22 12:12:57 +0200

    rajouté fonctionnalité logout

--- InstagramFirebaseAndroid/app/src/main/java/com/formation/instagramfirebaseandroid/ProfileActivity.kt
+++ InstagramFirebaseAndroid/app/src/main/java/com/formation/instagramfirebaseandroid/ProfileActivity.kt
@@ -3,25 +3,29 @@ package com.formation.instagramfirebaseandroid
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
 
-class PostsActivity : AppCompatActivity() {
+private const val TAG = "ProfileActivity"
+class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_posts)
+        setContentView(R.layout.activity_profile)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_posts, menu)
+        menuInflater.inflate(R.menu.menu_profile, menu)
         return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
-        if (item.itemId == R.id.menu_profile) {
-            val intent = Intent(this, ProfileActivity::class.java)
-            startActivity(intent)
+        if (item.itemId == R.id.menu_logout) {
+            Log.i(TAG, "L'utilisateur veut se déconnecter")
+            FirebaseAuth.getInstance().signOut()
+            startActivity(Intent(this, LoginActivity::class.java))
         }
 
         return super.onOptionsItemSelected(item)
