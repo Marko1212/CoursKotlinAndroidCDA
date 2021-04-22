66fcfb73c9e827c8dc1957e5597a4eb1e5fa4993
Marko1212 <shihonage7@gmail.com>
2021-04-22 09:47:06 +0200

    update

--- InstagramFirebaseAndroid/app/src/main/java/com/formation/instagramfirebaseandroid/LoginActivity.kt
+++ InstagramFirebaseAndroid/app/src/main/java/com/formation/instagramfirebaseandroid/LoginActivity.kt
@@ -2,10 +2,23 @@ package com.formation.instagramfirebaseandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        btnLogin.setOnClickListener {
+            val email = etEmail.text.toString()
+            val password = etPassword.text.toString()
+            if(email.isBlank() || password.isBlank()) {
+                Toast.makeText(this, "Email/mot de passe doivent être renseignés!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            // Vérification de l'authentification Firebase
+
+        }
     }
 }
