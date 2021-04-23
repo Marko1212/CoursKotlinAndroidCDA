d01ffbe1232992a54a148f98f206c1c546740793
Marko1212 <shihonage7@gmail.com>
2021-04-23 10:12:45 +0200

    upload d'un nouveau post

--- InstagramFirebaseAndroid/app/src/main/java/com/formation/instagramfirebaseandroid/CreateActivity.kt
+++ InstagramFirebaseAndroid/app/src/main/java/com/formation/instagramfirebaseandroid/CreateActivity.kt
@@ -9,6 +9,8 @@ import android.util.Log
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import com.formation.instagramfirebaseandroid.models.Post
 import com.formation.instagramfirebaseandroid.models.User
 import kotlinx.android.synthetic.main.activity_create.*
@@ -19,12 +21,12 @@ class CreateActivity : AppCompatActivity() {
     private var signedInUser: User? = null
     private var photoUri: Uri? = null
     private lateinit var firestoreDb: FirebaseFirestore
-
+    private lateinit var storageReference: StorageReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
-
+        storageReference = FirebaseStorage.getInstance().reference
         firestoreDb = FirebaseFirestore.getInstance()
         firestoreDb.collection("users")
             .document(FirebaseAuth.getInstance().currentUser?.uid as String)
@@ -38,7 +40,7 @@ class CreateActivity : AppCompatActivity() {
             }
 
         btnPickImage.setOnClickListener {
-            Log.i(TAG, "Ouvrir le sélecteur d'images sur l'appareil")
+            Log.i(TAG, "Ouvrir le sélectionneur d'images sur l'appareil")
             val imagePickerIntent = Intent(Intent.ACTION_GET_CONTENT)
             imagePickerIntent.type = "image/*"
             if (imagePickerIntent.resolveActivity(packageManager) != null) {
@@ -65,6 +67,38 @@ class CreateActivity : AppCompatActivity() {
             return
         }
 
+        btnSubmit.isEnabled = false
+        val photoUploadUri = photoUri as Uri
+        val photoReference = storageReference.child("images/${System.currentTimeMillis()}-photo.jpg")
+        // Upload de la photo à Firebase Storage
+        photoReference.putFile(photoUploadUri)
+            .continueWithTask { photoUploadTask ->
+                Log.i(TAG, "bytes uploadés : ${photoUploadTask.result?.bytesTransferred}")
+                // Récupérer image url de l'image uploadée
+                photoReference.downloadUrl
+            }.continueWithTask { downloadUrlTask ->
+                // Création de l'objet post avec image url et rajout du post à la collection des posts sur Firestore
+                val post = Post(
+                    etDescription.text.toString(),
+                    downloadUrlTask.result.toString(),
+                    System.currentTimeMillis(),
+                    signedInUser)
+                firestoreDb.collection("posts").add(post)
+            }.addOnCompleteListener { postCreationTask ->
+                btnSubmit.isEnabled = true
+                if (!postCreationTask.isSuccessful) {
+                    Log.e(TAG, "Exception pendant les opérations Firebase", postCreationTask.exception)
+                    Toast.makeText(this, "Echec de l'enregistrement du post", Toast.LENGTH_SHORT).show()
+                }
+                etDescription.text.clear()
+                imageView.setImageResource(0)
+                Toast.makeText(this, "Succès!", Toast.LENGTH_SHORT).show()
+                val profileIntent = Intent(this, ProfileActivity::class.java)
+                profileIntent.putExtra(EXTRA_USERNAME, signedInUser?.username)
+                startActivity(profileIntent)
+                finish()
+            }
+
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -75,7 +109,7 @@ class CreateActivity : AppCompatActivity() {
                 Log.i(TAG, "photoUri $photoUri")
                 imageView.setImageURI(photoUri)
             } else {
-                Toast.makeText(this, "Action du sélecteur d'images annulée", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Action du sélectionneur d'images annulée", Toast.LENGTH_SHORT).show()
             }
         }
     }
